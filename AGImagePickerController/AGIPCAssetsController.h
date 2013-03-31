9433377bf1a331c4e5afec598a68ff3c2eb836d0
mikechen <tap135@Hotmail.com>
2013-03-31 09:42:37 -0400

    Email feature

---
+++ AGImagePickerController/AGIPCAssetsController.h
@@ -0,0 +1,31 @@
+//
+//  AGIPCAssetsController.h
+//  AGImagePickerController
+//
+//  Created by Artur Grigor on 17.02.2012.
+//  Copyright (c) 2012 - 2013 Artur Grigor. All rights reserved.
+//  
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
+//  
+
+#import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+
+#import "AGImagePickerController.h"
+#import "AGIPCGridItem.h"
+
+
+//testemail
+#import <MessageUI/MFMailComposeViewController.h>
+
+
+@interface AGIPCAssetsController : UITableViewController<UITableViewDataSource, UITableViewDelegate, AGIPCGridItemDelegate, MFMailComposeViewControllerDelegate>
+
+@property (strong) ALAssetsGroup *assetsGroup;
+@property (ag_weak, readonly) NSArray *selectedAssets;
+@property (strong) AGImagePickerController *imagePickerController;
+
+- (id)initWithImagePickerController:(AGImagePickerController *)imagePickerController andAssetsGroup:(ALAssetsGroup *)assetsGroup;
+
+@end
