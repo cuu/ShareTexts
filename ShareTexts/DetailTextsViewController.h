5f9532d3cae02222adea06ad4e7bd594529e4852
suavros <stavros81@gmail.com>
2013-04-19 01:26:41 -0400

    added detail texts view controller

---
+++ ShareTexts/DetailTextsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTextsViewController.h
+//  ShareTexts
+//
+//  Created by stavro on 4/18/13.
+//  Copyright (c) 2013 stavro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTextsViewController : UIViewController
+
+@end
---
+++ ShareTexts/DetailTextsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailTextsViewController.m
+//  ShareTexts
+//
+//  Created by stavro on 4/18/13.
+//  Copyright (c) 2013 stavro. All rights reserved.
+//
+
+#import "DetailTextsViewController.h"
+
+@interface DetailTextsViewController ()
+
+@end
+
+@implementation DetailTextsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
